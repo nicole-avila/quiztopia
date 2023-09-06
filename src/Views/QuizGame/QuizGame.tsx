2f897e1c8a06c576f5941387bb9697b678c75379
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-09-06 15:13:32 +0200

    navigate from QG to Landing

--- src/Views/QuizGame/QuizGame.tsx
+++ src/Views/QuizGame/QuizGame.tsx
@@ -1,8 +1,14 @@
+import "./QuizGame.scss";
 import QuizList from "../../components/QuizList/QuizList";
+import { useNavigate } from "react-router-dom";
 
 export default function QuizGame() {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="quiz-game">
+      <h1 onClick={() => navigate("/")} className="quiz-game__title">
+        quiztopia
+      </h1>
       <QuizList />
     </div>
   );
